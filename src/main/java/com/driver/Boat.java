975d4edd08103e3ae056250e061b31bdd3ea29fb
kuruvasaikiran11 <kuruvasaikiran11@gmail.com>
2022-12-24 23:10:10 +0530

    Sixth Commit

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -5,13 +5,13 @@ public class Boat implements WaterVehicle{
     private String name;
     private int capacity;
 
-    public void setName(String name) {
+    /*public void setName(String name) {
         this.name = name;
     }
 
     public void setCapacity(int capacity) {
         this.capacity = capacity;
-    }
+    }*/
 
 
     public String getVehicleName() {
