833938dace95305d0579009d55f68e94de485e42
kuruvasaikiran11 <kuruvasaikiran11@gmail.com>
2022-12-24 23:03:54 +0530

    fourth Commit

--- src/main/java/com/driver/Vehicle.java
+++ src/main/java/com/driver/Vehicle.java
@@ -16,6 +16,26 @@ public class Vehicle {
 
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCurrentSpeed() {
+        return currentSpeed;
+    }
+
+    public void setCurrentSpeed(int currentSpeed) {
+        this.currentSpeed = currentSpeed;
+    }
+
+    public void setCurrentDirection(int currentDirection) {
+        this.currentDirection = currentDirection;
+    }
+
     public void steer(int direction){
         //direction is in degrees, add it to the current direction
         currentDirection += direction;
