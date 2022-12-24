8389f1e71095c7df9c914dd47a4cdeee8a3223ba
kuruvasaikiran11 <kuruvasaikiran11@gmail.com>
2022-12-24 23:07:49 +0530

    Fifth Commit

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -2,31 +2,23 @@ package com.driver;
 
 public class Boat implements WaterVehicle{
 
-    String name;
-    int capacity;
-
-    public String getName() {
-        return name;
-    }
+    private String name;
+    private int capacity;
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    public int getCapacity() {
-        return capacity;
     }
 
     public void setCapacity(int capacity) {
         this.capacity = capacity;
     }
 
-    @Override
+
     public String getVehicleName() {
         return name;
     }
 
-    @Override
+
     public int getVehicleCapacity() {
         return capacity;
     }
