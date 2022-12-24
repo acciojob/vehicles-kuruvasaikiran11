6569377e48d163aad184257618c01a7443b070c8
kuruvasaikiran11 <kuruvasaikiran11@gmail.com>
2022-12-24 23:00:26 +0530

    Third Commit

--- src/main/java/com/driver/Car.java
+++ src/main/java/com/driver/Car.java
@@ -9,7 +9,7 @@ public class Car extends Vehicle {
     private int currentGear;
     private int seats;
     public Car(){
-g
+
     }
     public Car(String name, int wheels, int doors, int gears, boolean isManual, String type, int seats) {
         super(name);
